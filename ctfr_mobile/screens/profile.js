71590ed463a1784dc9a66039a46e9d839499d118
lucitian <flippery.slippery28@gmail.com>
2021-11-06 16:13:17 +0800

    complicated push

---
+++ ctfr_mobile/screens/profile.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Alert, ImageBackground } from 'react-native';
+
+function ProfileScreen({navigation}) {
+    return(
+        <View>
+            <Text> Rovic! </Text>
+        </View>
+    )
+        
+    
+   
+}
+
+export default ProfileScreen
