105d1e37b0695bb186f5582aad224c720237027b
lucitian <flippery.slippery28@gmail.com>
2022-01-08 14:26:33 +0800

    fixed open camera disabling

--- ctfr_desktop/script/camera.js
+++ ctfr_desktop/script/camera.js
@@ -3,9 +3,13 @@ const openCamera = document.getElementById('camera__screen')
 function open__camera() {
     open_cam = fetch('http://localhost:5000/open_cam')
     openCamera.innerHTML += `<img src="http://localhost:5000/open_cam" height="100%" width="100%">`
+
+    document.getElementById('open__camera').disabled = true
 }
 
 function close__camera() {
     open_cam = fetch('http://localhost:5000/close_cam')
     openCamera.innerHTML = ''
+
+    document.getElementById('open__camera').disabled = false
 }
