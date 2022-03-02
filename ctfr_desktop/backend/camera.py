7600217800cbd888b8db0aa3a804c0c1ef2bb0d3
Mercovsk <marcbaja08@gmail.com>
2022-03-02 21:42:51 +0800

    face detection

--- ctfr_desktop/backend/camera.py
+++ ctfr_desktop/backend/camera.py
@@ -12,8 +12,22 @@ class camera(object):
         cv2.destroyAllWindows()
 
     def get_frame(self):
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+        
         while True:
             success, frame = self.video.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y +h), (255, 0, 0), 5)
+                roi_grey = gray[y:y+w, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_grey, 1.3, 5)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
             if not success:
                 break
